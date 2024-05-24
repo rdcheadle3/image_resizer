93addfd63c4aa4685a4a4ab3e4573dd1ef216420
rdcheadle3 <robert.cheadle01@gmail.com>
2024-05-24 10:13:35 -0400

    basic image resizer program works

--- image_resizer.py
+++ image_resizer.py
@@ -1,16 +1,32 @@
 import os
+from PIL import Image
+import config
 
 def print_files_in_dir(directory):
-    print("Checking directory...")  # Debug statement
     if not os.path.exists(directory):
         print(f"Directory does not exist: {directory}")
+        return []
     else:
-        print(f"Directory exists: {directory}")
         files = os.listdir(directory)
-        print(f"Files in directory: {files}")
+        image_files = []
+        for file in files:
+            if file.lower().endswith(".jpg"):
+                image_files.append(os.path.join(directory, file))
+        return image_files
+
+# read image file
+def resize_images(image_files, output_dir, size=(300, 300)):
+    for file in image_files:
+        try:
+            with Image.open(file) as img:
+                img_resized = img.resize(size, Image.LANCZOS)
+                base_name = os.path.basename(file)
+                new_file_path = os.path.join(output_dir, base_name)
+                img_resized.save(new_file_path)
+                print(f"Resized {file} and saved to {new_file_path}")
+        except Exception as e:
+            print(f"Error opening image {file}: {e}")
 
 if __name__ == "__main__":
-    print("Script is running...")  # Debug statement
-    directory = "/home/rcheadle/workspace/github.com/rdcheadle3/image_resizer/tools/"  # Replace with your directory path
-    print_files_in_dir(directory)
-    print("Script completed.")  # Debug statement
+    image_files = print_files_in_dir(config.IMAGE_DIR)
+    resize_images(image_files, config.RESIZED_DIR)
